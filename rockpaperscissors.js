e36abf5fd9426f6881911680e06e61a7ed4b6391
DerOeko <samuelgerrit.nellessen@gmail.com>
2022-05-20 10:04:45 +0200

    Create game function to play game 5 times

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,66 +1,48 @@
-/* Create a function that chooses the stance of the computer.
-There are three stances, rock, paper or scissors. 
-Randomly assign a position to the computer.
-*/
-/*
-Create function.
-Create variable in which I can store the position of the computer.
-Randomly assign a number between 1 and 3 to this variable.
-Return this variable.
-*/
 function computerPlay(){
-    let randomNumber = 1;
-    let computerStance;
-    if((randomNumber = Math.floor(Math.random()*3)+1)==1){
-        computerStance = "rock";
-    }else if((randomNumber = Math.floor(Math.random()*3)+1)==2){
-        computerStance = "scissors";
-    }else{
-        computerStance = "paper";
-    };
-    return computerStance;
+    const stances = ["rock", "paper", "scissors"];
+    const arrayLength = stances.length;
+    const computerSelection = stances[Math.floor(Math.random()*stances.length)];
+    return computerSelection;
 }
 
-/* Create a function.
-Asks the user for their input, Rock, Scissor or Paper.
-Assigns this input to a variable, that is later compared to the one selected by the computer.*/
-function playerSelection(){
-    let selection;
-    selection = prompt("What stance do you choose?");
-    let playerStance = selection.toLowerCase()
-    return playerStance;
+function playerPlay(){
+    const playerSelection = prompt("What stance do you choose?").toLowerCase();
+    return playerSelection;
 }
 
-
-/* Create a function.
-Compare playerStance and computerStance and display message, that uses both positions.*/
-
-function displayVictory(){
-    
-    let playerScore;
-    let computerScore;
-    if (computerStance == "rock" && playerStance == "scissors"||computerStance== "paper" && playerStance == "rock"){
+function playRound(){
+    let computerSelection = computerPlay();
+    let playerSelection = playerPlay();
+    if(computerSelection === playerSelection){
+        alert(`Draw! The computer chose ${computerSelection} and you chose ${playerSelection}.`);
         return 0;
+    }else if(computerSelection !== playerSelection){
+        if(computerSelection === "rock" && playerSelection === "paper" || computerSelection === "paper" && playerSelection === "scissors" || computerSelection === "scissors" && playerSelection === "rock"){
+            alert(`You win! The computer chose ${computerSelection} and you chose ${playerSelection}.`);
+            return 1;
+        }else{
+            alert(`You loose! The computer chose ${computerSelection} and you chose ${playerSelection}.`);
+            return 2;
+        }
     }else{
-        return 1;
+        alert("Error");
     }
 }
 
 function game(){
-    let computerScore = 0;
     let playerScore = 0;
-    let computerStance = computerPlay();
-    let playerStance = playerSelection();
-    for (let i = 0; i<5; i++){
-        let result = displayVictory();
-        if (result = 0){
-            console.log(`Ỳou lose! The computer chose ${computerStance} and you chose ${playerStance}.`);
-            computerScore += 1;
-        }else if (result = 1){
-            console.log(`You win! The computer chose ${computerStance} and you chose ${playerStance}.`);
+    let computerScore = 0;
+    for(let i = 0; i<5; i++){
+        let gameOutcome = playRound();
+
+        if(gameOutcome===0){
+            continue;
+        }else if(gameOutcome===1){
             playerScore += 1;
         }else{
-            console.log(`Stalemate! You both chose ${computerStance}.`)
+            computerScore += 1;
         }
     }
+    console.log(computerScore);
+    console.log(playerScore);
 }
