5e987f2f06ae71a82b8cdba7c49a061b83ecb58a
DerOeko <samuelgerrit.nellessen@gmail.com>
2022-05-21 08:36:17 +0200

    Add helper functions, displayScore and checkWin

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -10,39 +10,77 @@ function playerPlay(){
     return playerSelection;
 }
 
-function playRound(){
+function playRound(playerSelection){
     let computerSelection = computerPlay();
-    let playerSelection = playerPlay();
     if(computerSelection === playerSelection){
-        alert(`Draw! The computer chose ${computerSelection} and you chose ${playerSelection}.`);
-        return 0;
+        displayScore("draw");
+        results.textContent = `Draw! The computer chose ${computerSelection} and you chose ${playerSelection}.`;
     }else if(computerSelection !== playerSelection){
         if(computerSelection === "rock" && playerSelection === "paper" || computerSelection === "paper" && playerSelection === "scissors" || computerSelection === "scissors" && playerSelection === "rock"){
-            alert(`You win! The computer chose ${computerSelection} and you chose ${playerSelection}.`);
-            return 1;
+            displayScore("win");
+            results.textContent = `You win! The computer chose ${computerSelection} and you chose ${playerSelection}.`;
         }else{
-            alert(`You loose! The computer chose ${computerSelection} and you chose ${playerSelection}.`);
-            return 2;
+            displayScore("loose");
+            results.textContent = `You loose! The computer chose ${computerSelection} and you chose ${playerSelection}.`;
         }
     }else{
         alert("Error");
     }
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i<5; i++){
-        let gameOutcome = playRound();
+let playerScore = 0;
+let computerScore = 0;
 
-        if(gameOutcome===0){
-            continue;
-        }else if(gameOutcome===1){
+function displayScore(outcome){
+        if(outcome === "draw"){
+            playerScore += 0;
+            computerScore += 0;
+            score.textContent = `Your current score is ${playerScore}. The computer's score is ${computerScore}.`;
+        }else if(outcome === "win"){
             playerScore += 1;
-        }else{
+            score.textContent = `Your current score is ${playerScore}. The computer's score is ${computerScore}.`;
+            checkWin(playerScore);
+        }else if(outcome === "loose"){
             computerScore += 1;
+            score.textContent = `Your current score is ${playerScore}. The computer's score is ${computerScore}.`;
+            checkWin(computerScore);
+        }
+}
+
+function checkWin(number){
+    if(playerScore === 5 ||computerScore === 5){
+        if(playerScore === 5){
+            score.textContent = `You won! Your score is ${playerScore}. The computer's score is ${computerScore}.`;
+            playerScore = 0;
+            computerScore = 0;
+        }else if(computerScore === 5){
+            score.textContent = `The computer won... Your score is ${playerScore}. The computer's score is ${computerScore}.`;
+            playerScore = 0;
+            computerScore = 0;
         }
     }
-    console.log(computerScore);
-    console.log(playerScore);
 }
+
+const headline = document.querySelector("h1");
+const buttons = document.querySelectorAll("button");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const results = document.querySelector("#results");
+const score = document.querySelector("#score");
+
+buttons.forEach((button) => {
+    button.classList.add("btn");
+})
+
+rock.addEventListener("click", () => {
+    playRound("rock")
+})
+paper.addEventListener("click", ()=>{
+    playRound("paper");
+})
+scissors.addEventListener("click", ()=>{
+    playRound("scissors");
+})
+
+
